bc7d7f916d8e19c38333089f418147a12ddc0409
Huibo Zhao <zhaohuibo95@gmail.com>
2020-11-28 15:07:26 -0800

    add solution

---
+++ LC1664_Ways_To_Make_A_Fair_Array/solution.java
@@ -0,0 +1,53 @@
+// smart solution by @lee215 from leetcode reference
+public int waysToMakeFair(int[] A) {
+    int res = 0, n = A.length, left[] = new int[2], right[] = new int[2];
+    for (int i = 0; i < n; i++)
+        right[i%2] += A[i]; 
+    for (int i = 0; i < n; i++) {
+        right[i%2] -= A[i];
+        if (left[0]+right[1] == left[1]+right[0]) res++;
+        left[i%2] += A[i];
+    }
+    return res;
+}
+
+// my own not smart solution
+class Solution {
+    public int waysToMakeFair(int[] nums) {
+        int evenIndexSum = 0;
+        int oddIndexSum = 0;
+        int[] evenIndexCumSum = new int[nums.length];
+        int[] oddIndexCumSum = new int[nums.length];
+        
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (i % 2 == 0) {
+                evenIndexSum += nums[i];
+                evenIndexCumSum[i] += nums[i];
+                if (i + 2 < nums.length)
+                    evenIndexCumSum[i] += evenIndexCumSum[i + 2];
+            } else {
+                oddIndexSum += nums[i];
+                oddIndexCumSum[i] += nums[i];
+                if (i + 2 < nums.length)
+                    oddIndexCumSum[i] += oddIndexCumSum[i + 2];
+            }
+        }
+        
+        int res = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                int temp = (i + 1) < nums.length ? oddIndexCumSum[i + 1] : 0;
+                if (evenIndexSum - 2 * evenIndexCumSum[i] + 2 * temp == oddIndexSum - nums[i])
+                    res++;
+            } else {
+                int temp = (i + 1) < nums.length ? evenIndexCumSum[i + 1] : 0;
+                if (oddIndexSum - 2 * oddIndexCumSum[i] + 2 * temp == evenIndexSum - nums[i])
+                    res++;                
+            }
+        }
+        
+        
+        return res;
+    }
+}
